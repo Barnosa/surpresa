4eb90c00be048edad283a959772064c1d5f44130
Kauã Barbosa <130697251+Barnosa@users.noreply.github.com>
2024-10-15 19:54:20 -0300

    Add files via upload

---
+++ mensagens.php
@@ -0,0 +1,8 @@
+<?php
+$mensagens = [
+    ["mensagem" => "Amo você meu amor !", "data" => "2023-01-01"],
+    ["mensagem" => "Amo cada momento que passo com você.", "data" => "2023-02-14"],
+    ["mensagem" => "Você faz meu coração sorrir.", "data" => "2023-03-01"],
+    // Adicione mais mensagens aqui
+];
+?>
